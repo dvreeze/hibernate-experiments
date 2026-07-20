349fab3ad67add213845c2db711e7b4b8d101a63
Chris de Vreeze <chris.de.vreeze@caiway.net>
2026-07-20 17:42:44 +0200

    Enhanced API documentation in entity package-info

--- em-criteria/service/src/main/java/eu/cdevreeze/hibernateexperiments/emcriteria/entity/package-info.java
+++ em-criteria/service/src/main/java/eu/cdevreeze/hibernateexperiments/emcriteria/entity/package-info.java
@@ -18,6 +18,16 @@
  * JPA {@link jakarta.persistence.Entity} classes. They are internal implementation details of
  * the service layer.
  * <p>
+ * All entity associations have been configured to use lazy fetching. Fetching behavior is chosen per
+ * query on an ad-hoc basis.
+ * <p>
+ * The owning side of associations is the side that contains the foreign key. So that would be the
+ * ManyToOne side rather than the OneToMany side (if not OneToOne or ManyToMany). As a consequence,
+ * the owning side is often the side opposite to the relationship as "aggregation". For example,
+ * an order has line items, not the other way around, but a line item would have a foreign key to the order containing it.
+ * Again, this shows the importance of lazy fetching by default. Also note that the immutable model
+ * record classes are closer to "aggregations".
+ * <p>
  * Note the {@link org.jspecify.annotations.NullUnmarked} annotation, since nullability is everywhere in JPA entities.
  * In JPA entities, optionality of entity fields is expressed using JPA annotations.
  *
