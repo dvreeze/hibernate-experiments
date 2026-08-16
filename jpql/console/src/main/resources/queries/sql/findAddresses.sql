98d83e3c8fb9c178b626404da9129558ac8fab90
Chris de Vreeze <chris.de.vreeze@caiway.net>
2026-08-16 21:15:44 +0200

    Minor change to SQL script

--- jpql/console/src/main/resources/queries/sql/findAddresses.sql
+++ jpql/console/src/main/resources/queries/sql/findAddresses.sql
@@ -1,5 +1,5 @@
 select a.address_id, a.address, a.address2, a.district, a.city_id, a.postal_code, a.phone,
-       c.city, c.country_id, co.country
+       ct.city, ct.country_id, co.country
   from Address a
-  join City c on (a.city_id = c.city_id)
-  join Country co on (c.country_id = co.country_id)
+  join City ct on (a.city_id = ct.city_id)
+  join Country co on (ct.country_id = co.country_id)
