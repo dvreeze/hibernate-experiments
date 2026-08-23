06bc4160adb5954029b2872bd95bba5be4422d07
Chris de Vreeze <chris.de.vreeze@caiway.net>
2026-08-23 22:02:04 +0200

    Added program to find films (to plain-sql)

---
+++ plain-sql/console/src/main/java/eu/cdevreeze/hibernateexperiments/plainsql/console/FindAllFilmsUsingCtes.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2026-2026 Chris de Vreeze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cdevreeze.hibernateexperiments.plainsql.console;
+
+import module eu.cdevreeze.hibernateexperiments.plainsql.service;
+import module java.base;
+import jakarta.persistence.EntityManagerFactory;
+import tools.jackson.databind.json.JsonMapper;
+import tools.jackson.datatype.guava.GuavaModule;
+
+/**
+ * Program finding all films in the database, using CTEs in the implementation.
+ *
+ * @author Chris de Vreeze
+ */
+public class FindAllFilmsUsingCtes {
+
+    static void main(String... args) {
+        JsonMapper jsonMapper = JsonMapper.builder()
+                .addModule(new GuavaModule())
+                .build();
+
+        try (EntityManagerFactory emf = EntityManagerFactories.createEntityManagerFactory("pagila")) {
+            FilmService filmService = AlternativeFilmServiceFactory.create(emf);
+
+            List<Film> films = filmService.findAllFilms();
+
+            jsonMapper.writerWithDefaultPrettyPrinter().writeValue(System.out, films);
+        }
+    }
+}
