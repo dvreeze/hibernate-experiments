5c67e2b63d3388351e42671c5a04c3a602918eed
Chris de Vreeze <chris.de.vreeze@caiway.net>
2026-09-02 00:23:15 +0200

    Added some console programs to find films by actor ID

---
+++ em-criteria/console/src/main/java/eu/cdevreeze/hibernateexperiments/emcriteria/console/FindFilmsByActorIdUsingSeparateQueries.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2026-2026 Chris de Vreeze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cdevreeze.hibernateexperiments.emcriteria.console;
+
+import module eu.cdevreeze.hibernateexperiments.emcriteria.service;
+import module java.base;
+import jakarta.persistence.EntityManagerFactory;
+import tools.jackson.databind.json.JsonMapper;
+import tools.jackson.datatype.guava.GuavaModule;
+
+/**
+ * Program finding the films with a given actor (as ID), returning it with actors and categories.
+ * It uses {@link ConcreteFilmServiceUsingSeparateQueriesFactory}.
+ *
+ * @author Chris de Vreeze
+ */
+public class FindFilmsByActorIdUsingSeparateQueries {
+
+    static void main(String... args) {
+        JsonMapper jsonMapper = JsonMapper.builder()
+                .addModule(new GuavaModule())
+                .build();
+
+        Objects.checkIndex(0, args.length);
+        long actorId = Long.parseLong(args[0]);
+
+        try (EntityManagerFactory emf = EntityManagerFactories.createEntityManagerFactory("pagila")) {
+            FilmService filmService = ConcreteFilmServiceUsingSeparateQueriesFactory.create(emf);
+
+            List<Film> films = filmService.findFilmsByActorId(actorId);
+
+            jsonMapper.writerWithDefaultPrettyPrinter().writeValue(System.out, films);
+        }
+    }
+}
